47ed88180cae04663dfa581ee78042c17662af7b
luciapuccini <puccinilucia@gmail.com>
2018-09-07 10:53:34 -0300

    setup router v4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+//just for testing
+
+class Hello extends React.Component {
+  render() { return <div> Holis </div> }
+}
+class Goodbye extends React.Component {
+  render() {
+    return (
+      <div>
+        bye
+      </div>
+    )
+  }
+}
+
+
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
